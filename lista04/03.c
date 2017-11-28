0ce0435cc8951e8b1417dd328100f77eacd0b193
BrunoLad <bruno13dm@outlook.com>
2017-11-28 00:45:40 -0200

    Exercício 03 concluído.

--- lista04/03.c
+++ lista04/03.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define N 100
+#include <stdlib.h>
 
 typedef struct aluno{
     int matricula;
@@ -7,19 +7,43 @@ typedef struct aluno{
     float p2;
     float p3;
     float media;
-    char nome[N];
+    char nome[100];
 } aluno;
+
+float media_notas(aluno *a);
 
 int main(){
     
-    fscanf(fp, "\%s \%d \%f \%f \%f",
-                alunos[i].nome, &alunos[i].matricula, 
-                &alunos[i].p1, &aluno[i].p2, &aluno[i].p3);
-                
-    fprintf(fp, "\%s \%d \%.2f \%.2f \%.2f \%.2f\n"
-                alunos[i].nome, alunos[i]matricula,
-                alunos[i].p1, alunos[i].p2, alunos[i].p3
-                aluno[i].media);
+    FILE * fp = fopen("lista_alunos.txt", "r");
+    aluno alunos[50];
     
+    int i = 0;
+    
+    while(i < 50){
+        fscanf(fp, "\%s \%d \%f \%f %f",
+                    alunos[i].nome, &alunos[i].matricula, 
+                    &alunos[i].p1, &alunos[i].p2, &alunos[i].p3);
+        i++;
+    }    
+    
+    fclose(fp);
+    fp = fopen("media_alunos.txt", "w");
+    i = 0;
+    
+    while(i < 50){
+        alunos[i].media = media_notas(alunos + i);
+        
+        fprintf(fp, "\%s \%d \%.2f \%.2f \%.2f \%.2f\n",
+                alunos[i].nome, alunos[i].matricula,
+                alunos[i].p1, alunos[i].p2, alunos[i].p3,
+                alunos[i].media);
+        i++;
+    }
+    fclose(fp);
     return 0;
+}
+
+float media_notas(aluno *a)
+{
+    return (a->p1 + a->p2 + a->p3)/3.0;
 }
